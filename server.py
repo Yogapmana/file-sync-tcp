00c3f99dac438bfaebd42185be2f332511870f99
Yogapmana <yogapermana402@gmail.com>
2026-07-15 09:44:46 +0700

    Fix logging FileNotFoundError in server.py

--- server.py
+++ server.py
@@ -9,6 +9,8 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from pathlib import Path
 
+Path("logs").mkdir(exist_ok=True)
+
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] %(levelname)s - %(message)s',
